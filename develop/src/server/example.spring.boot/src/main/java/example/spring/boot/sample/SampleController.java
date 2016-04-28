7b69e83e3eecd46ba353165f6c7274fff92e278a
weixin <liuxiamai@163.com>
2016-04-28 11:38:07 +0800

    add maven compile config

--- develop/src/server/example.spring.boot/src/main/java/example/spring/boot/sample/SampleController.java
+++ develop/src/server/example.spring.boot/src/main/java/example/spring/boot/sample/SampleController.java
@@ -2,12 +2,14 @@ package example.spring.boot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@ComponentScan("example.spring.boot.sample")
 @EnableAutoConfiguration
 public class SampleController {
 
