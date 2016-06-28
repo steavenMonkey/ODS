8cd92055c3e9e4caab513cc2c54b078dc166592b
weixin <liuxiamai@163.com>
2016-06-29 01:24:42 +0800

    add thymeleaf example

---
+++ develop/src/server/example.spring.boot/src/main/java/example/spring/boot/thymeleaf/SampleController.java
@@ -0,0 +1,33 @@
+package example.spring.boot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ComponentScan("example.spring.boot.thymeleaf")
+@EnableAutoConfiguration
+public class SampleController {
+
+	/*
+	 * 所有Thymeleaf的相关依赖都会被加载到类路径中，
+	 * 更重要的是，上文中所有TemplateEngine，
+	 * ThymeleafViewResolver等bean都在应用启动后被自动实例化，
+	 * 默认情况下模板的目录位于src/main/resources/templates/文件夹中，
+	 * 所以开发者只需要在这个文件夹中添加模板文件即可。
+	 */
+	
+    @RequestMapping("/")
+//    @ResponseBody 这个表示以他自己为返回值
+    String hello() {
+        return "hello";
+    }
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
